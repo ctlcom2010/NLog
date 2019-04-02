0dd88512742ddf6a1397533ccba50b6bb6bcd5dc
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-04-03 00:46:46 +0200

    WIP

--- tests/NLog.UnitTests/Internal/NetworkSenders/HttpNetworkSenderTests.cs
+++ tests/NLog.UnitTests/Internal/NetworkSenders/HttpNetworkSenderTests.cs
@@ -1,19 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Authentication;
+using NLog.Config;
+using NLog.Internal.NetworkSenders;
+using NLog.Targets;
+using NLog.UnitTests.Mocks;
 using Xunit;
 
 namespace NLog.UnitTests.Internal.NetworkSenders
 {
-    class HttpNetworkSenderTests : NLogTestBase
+    public class HttpNetworkSenderTests : NLogTestBase
     {
         [Fact]
         public void HttpHappyPathTest()
         {
+            // Arrange
+            var networkTarget = new NetworkTarget("target1");
+            networkTarget.Address = "http://test.with.mock";
+            var senderFactoryWithHttpMocks = new SenderFactoryWithHttpMocks();
+            networkTarget.SenderFactory = senderFactoryWithHttpMocks;
+
+            var logFactory = new LogFactory();
+            var config = new LoggingConfiguration(logFactory);
+            config.AddRuleForAllLevels(networkTarget);
+            logFactory.Configuration = config;
+
+            var logger = logFactory.GetLogger("HttpHappyPathTestLogger");
+
+            // Act
+            logger.Info("test message1");
+            logFactory.Flush();
+
+            // Assert
+            var mock = senderFactoryWithHttpMocks.WebRequestMock;
+            var s = mock.GetRequestContentAsString();
+            var s2 = mock.GetResponseContentAsString();
+         //   var s3 = mock.GetResponse();
+
+
+            Assert.Equal("http://test.with.mock", mock.RequestedAddress.ToString());
 
         }
 
+    }
+
+    internal class SenderFactoryWithHttpMocks : INetworkSenderFactory
+    {
+        #region Implementation of INetworkSenderFactory
+
+        /// <inheritdoc />
+        public NetworkSender Create(string url, int maxQueueSize, SslProtocols sslProtocols, TimeSpan keepAliveTime)
+        {
+            return new HttpNetworkSender(url)
+            {
+                WebRequestFactory = new WebRequestFactoryMock(WebRequestMock)
+            };
+        }
+
+        public WebRequestMock WebRequestMock => new WebRequestMock();
+
+        #endregion
     }
 }
--- tests/NLog.UnitTests/Mocks/WebRequestMock.cs
+++ tests/NLog.UnitTests/Mocks/WebRequestMock.cs
@@ -4,75 +4,146 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NLog.UnitTests.Mocks
 {
-    //public class WebRequestMock : WebRequest
-    //{
-    //    public Uri RequestedAddress { get; set; }
-
-    //    public MemoryStream WrittenStream = new MemoryStream();
-
-    //    #region Overrides of WebRequest
-
-    //    /// <inheritdoc />
-    //    public override IAsyncResult BeginGetResponse(AsyncCallback callback, object state)
-    //    {
-    //        return base.BeginGetResponse(callback, state);
-    //    }
-
-    //    /// <inheritdoc />
-    //    public override WebResponse EndGetResponse(IAsyncResult asyncResult)
-    //    {
-    //        return base.EndGetResponse(asyncResult);
-    //    }
-
-    //    /// <inheritdoc />
-    //    public override IAsyncResult BeginGetRequestStream(AsyncCallback callback, object state)
-    //    {
-    //        return base.BeginGetRequestStream(callback, state);
-    //    }
-
-    //    /// <inheritdoc />
-    //    public override Stream EndGetRequestStream(IAsyncResult asyncResult)
-    //    {
-    //        return WrittenStream;
-    //    }
-
-    //    #endregion
-    //}
-
-    class WebRequestMock : WebRequest
+    public class WebRequestMock : WebRequest
     {
-        readonly MemoryStream _requestStream = new MemoryStream();
-        readonly MemoryStream _responseStream;
-
-        public override string Method { get; set; }
-        public override string ContentType { get; set; }
-        public override long ContentLength { get; set; }
         public Uri RequestedAddress { get; set; }
 
-        public WebRequestMock(string response)
+        public MemoryStream WrittenStream = new MemoryStream();
+
+
+
+        #region Overrides of WebRequest
+
+        ///// <inheritdoc />
+        //public override IAsyncResult BeginGetResponse(AsyncCallback callback, object state)
+        //{
+        //    return new AsyncResultMock(state);
+        //}
+
+        /// <inheritdoc />
+        public override WebResponse EndGetResponse(IAsyncResult asyncResult)
         {
-            _responseStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(response));
+            var responseStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("new response 1"));
+            return new WebReponseMock(responseStream);
         }
 
-        public string ContentAsString()
+        /// <inheritdoc />
+        public override IAsyncResult BeginGetRequestStream(AsyncCallback callback, object state)
         {
-            return System.Text.Encoding.UTF8.GetString(_requestStream.ToArray());
+          
+            var result = new AsyncResultMock(state);
+            callback(result);
+            return result;
         }
 
-        public override Stream GetRequestStream()
+        /// <inheritdoc />
+        public override Stream EndGetRequestStream(IAsyncResult asyncResult)
         {
-            return _requestStream;
+            return WrittenStream;
         }
 
-        public override WebResponse GetResponse()
+
+        public override IAsyncResult BeginGetResponse(AsyncCallback callback, object state)
         {
-            return new WebReponseMock(_responseStream);
+            var result = new AsyncResultMock(state);
+            callback(result);
+            return result;
+            //Task<WebResponse> f = Task<WebResponse>.Factory.StartNew(
+            //    c =>
+            //    {
+            //        var responseStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("new response 2"));
+            //        return new WebReponseMock(responseStream);
+            //    },
+            //    state
+            //);
+
+            //if (callback != null) f.ContinueWith((res) => callback(f));
+            //return f;
+        }
+        //public override WebResponse EndGetResponse(IAsyncResult asyncResult)
+        //{
+        //    return ((Task<WebResponse>)asyncResult).Result;
+        //}
+
+        /// <inheritdoc />
+        public override string Method { get; set; }
+
+        #endregion
+
+        public string GetRequestContentAsString()
+        {
+            return System.Text.Encoding.UTF8.GetString(WrittenStream.ToArray());
+        }
+        public string GetResponseContentAsString()
+        {
+            return "todo";
+            //return System.Text.Encoding.UTF8.GetString(_responseStream.ToArray());
         }
     }
+
+    class AsyncResultMock : IAsyncResult
+    {
+        public AsyncResultMock(object state)
+        {
+            AsyncState = state;
+        }
+
+        #region Implementation of IAsyncResult
+
+        /// <inheritdoc />
+        public object AsyncState { get; set; }
+
+        /// <inheritdoc />
+        public WaitHandle AsyncWaitHandle { get; set; }
+
+        /// <inheritdoc />
+        public bool CompletedSynchronously { get; set; }
+
+        /// <inheritdoc />
+        public bool IsCompleted { get; set; }
+
+        #endregion
+    }
+
+    //class WebRequestMock : WebRequest
+    //{
+    //    readonly MemoryStream _requestStream = new MemoryStream();
+    //    readonly MemoryStream _responseStream;
+
+    //    public override string Method { get; set; }
+    //    public override string ContentType { get; set; }
+    //    public override long ContentLength { get; set; }
+    //    public Uri RequestedAddress { get; set; }
+
+    //    public WebRequestMock(string response)
+    //    {
+    //        _responseStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(response));
+    //    }
+
+    //    public string GetRequestContentAsString()
+    //    {
+    //        return System.Text.Encoding.UTF8.GetString(_requestStream.ToArray());
+    //    }  
+    //    public string GetResponseContentAsString()
+    //    {
+    //        return System.Text.Encoding.UTF8.GetString(_responseStream.ToArray());
+    //    }
+
+    //    public override Stream GetRequestStream()
+    //    {
+    //        return _requestStream;
+    //    }
+
+    //    public override WebResponse GetResponse()
+    //    {
+    //        return new WebReponseMock(_responseStream);
+    //    }
+    //}
 
     class WebReponseMock : WebResponse
     {
